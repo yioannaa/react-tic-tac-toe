22f2136f25495e8b59302070ee004489927c5fce
Joanna <krzyzanowska.joannaa@gmail.com>
2020-11-02 23:39:50 +0100

    delete GameStyles, add inline styling, add Board and Square components

--- src/index.js
+++ src/index.js
@@ -3,17 +3,40 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 
+const Square = ()=>{
+  return (
+    <div style={{
+      backgroundColor: 'gold',
+      margin: 10,
+      padding: 20,
+    }}>
+      Square
+      </div>
+  );
+};
 
-const gameStyles = {
-  backgroundColor: 'salmon',
-  margin: 10,
-  padding: 20,
+const Board = ()=>{
+  return (
+    <div style={{
+      backgroundColor: 'skyblue',
+      margin: 10,
+      padding:20,
+    }}>
+      Board
+      <Square />
+      </div>
+  );
 };
 
 const Game = () => {
   return (
-    <div>
+    <div style={{
+      backgroundColor: 'salmon',
+      margin: 10,
+      padding: 20,
+    }}>
       Game
+      <Board />
     </div>
   );
 };
