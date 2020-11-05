d2f7d1ddd95fdffd4a31b45cb76b0c4cf82da473
Joanna <krzyzanowska.joannaa@gmail.com>
2020-11-05 16:03:05 +0100

    state in board instead of square - lifting state up

--- src/index.js
+++ src/index.js
@@ -4,21 +4,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 const Square = (props)=>{
-  const [value, setValue] = useState(null);
   return (
     <
     button className="square"
-    onClick={()=> setValue('X')}
+    onClick={()=> {}}
     >
-      {value}
+      {props.value}
       </button>
   );
 };
 
 const Board = ()=>{
+  const initialSquares = [
+    null, null, null,
+    null, null, null,
+    null, null, null
+  ];
+  const [squares, setSquares] = useState(initialSquares);
   const renderSquare = (i)=>{
     return(
-      <Square/>
+      <Square value={squares[i]}/>
     );
   };
   return (
