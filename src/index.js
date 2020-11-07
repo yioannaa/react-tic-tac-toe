8715d3ba28ff48bdedf698e9e905e9ea6a43f376
Joanna <krzyzanowska.joannaa@gmail.com>
2020-11-07 23:08:37 +0100

    add calculatewinner function

--- src/index.js
+++ src/index.js
@@ -34,7 +34,10 @@ const Board = ()=>{
     );
   };
 
-const status = `Next player: ${xIsNext ? 'X' : 'O'}`;
+const winner = calculateWinner(squares);
+const status = winner ?
+  `Winner: ${winner}` :
+ `Next player: ${xIsNext ? 'X' : 'O'}`;
 
   return (
     <div>
@@ -65,3 +68,20 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
+
+function calculateWinner (squares){
+  const lines = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4,6]
+  ];
+
+  for (let line of lines){
+    const [a, b, c] = line;
+
+    if (squares[a] && squares[a]===squares[b] && squares[a]===squares[c]){
+      return squares[a];
+    }
+  }
+  return null;
+}
